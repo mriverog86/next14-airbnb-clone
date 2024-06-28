e9a7b8bcb3e5ffec107ee3f5f62cc00691e6e1f9
Magdiel Rivero González <desarrollo5@hab.eicma.cu>
2024-06-28 18:28:50 -0400

    [FIX] DinamicServerError

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,33 @@ import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
 
-import getListings, { 
-  IListingsParams
-} from "@/app/actions/getListings";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
+import { DynamicServerError } from "next/dist/client/components/hooks-server-context";
 
 interface HomeProps {
-  searchParams: IListingsParams
-};
+  searchParams: IListingsParams;
+}
 
 const Home = async ({ searchParams }: HomeProps) => {
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
+  try {
+    const listings = await getListings(searchParams);
+    const currentUser = await getCurrentUser();
 
-  if (listings.length === 0) {
+    if (listings.length === 0) {
+      return (
+        <ClientOnly>
+          <EmptyState showReset />
+        </ClientOnly>
+      );
+    }
+
     return (
       <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    );
-  }
-
-  return (
-    <ClientOnly>
-      <Container>
-        <div 
-          className="
+        <Container>
+          <div
+            className="
             pt-24
             grid 
             grid-cols-1 
@@ -39,18 +39,22 @@ const Home = async ({ searchParams }: HomeProps) => {
             2xl:grid-cols-6
             gap-8
           "
-        >
-          {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
-        </div>
-      </Container>
-    </ClientOnly>
-  )
-}
+          >
+            {listings.map((listing: any) => (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            ))}
+          </div>
+        </Container>
+      </ClientOnly>
+    );
+
+  } catch (error: any) {
+    return null;
+  }
+};
 
 export default Home;
