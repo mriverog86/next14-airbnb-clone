7b4a162eb4b780b8a4d2c275cacc931e526fc4ae
Magdiel Rivero González <desarrollo5@hab.eicma.cu>
2024-06-27 23:22:51 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+    ]
+  }
+}
+
+module.exports = nextConfig
